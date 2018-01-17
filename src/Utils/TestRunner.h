5b90dd6d821f8a6153a7559cd4eac78ef3061878
KonH <konh@yandex.ru>
2018-01-17 19:46:00 +0700

    TestRunner refactoring;

--- src/Utils/TestRunner.h
+++ src/Utils/TestRunner.h
@@ -55,40 +55,51 @@ namespace Utils {
 	}
 
 	template<class T, class U>
-	void AssertEqual(const T& t, const U& u, const string& hint) {
+	void AssertEqual(const T& t, const U& u, const string& hint = "") {
 		if ( t != u ) {
 			ostringstream os;
-			os << "Assertion failed: " << t << " != " << u << " hint: " << hint;
+			os << "Assertion failed: " << t << " != " << u;
+			if ( !hint.empty() ) {
+				os << " hint: " << hint;
+			}
 			throw runtime_error(os.str());
 		}
 	}
 
-	void Assert(bool b, const string& hint) {
+	void Assert(bool b, const string& hint = "") {
 		AssertEqual(b, true, hint);
 	}
 
 	class TestRunner {
 	public:
+		TestRunner(const string& prefix = "") :
+				_prefix(prefix) {
+		}
+
 		template<class TestFunc>
-		void RunTest(TestFunc func, const string& test_name) {
+		void RunTest(TestFunc func, const string& testName) {
 			try {
 				func();
-				cerr << test_name << " OK" << endl;
+				if ( !_prefix.empty() ) {
+					cerr << _prefix << ".";
+				}
+				cerr << testName << " OK" << endl;
 			} catch ( runtime_error& e ) {
-				++fail_count;
-				cerr << test_name << " fail: " << e.what() << endl;
+				++_failCount;
+				cerr << testName << " fail: " << e.what() << endl;
 			}
 		}
 
 		~TestRunner() {
-			if ( fail_count > 0 ) {
-				cerr << fail_count << " unit tests failed. Terminate" << endl;
+			if ( _failCount > 0 ) {
+				cerr << _failCount << " unit tests failed. Terminate" << endl;
 				exit(1);
 			}
 		}
 
 	private:
-		int fail_count = 0;
+		const string _prefix;
+		int _failCount = 0;
 	};
 
 }
