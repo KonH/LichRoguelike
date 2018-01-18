775565266939c1c329f09b2cfb22a58ca7a87ec2
KonH <konh@yandex.ru>
2018-01-18 23:25:59 +0700

    Console to header only;

--- src/Utils/Console.h
+++ src/Utils/Console.h
@@ -1,10 +1,20 @@
 #pragma once
 
+#include <cstdlib>
+
+using namespace std;
+
 namespace Utils {
 
 	class Console {
 	public:
-		void Clear();
+		void Clear() {
+#ifdef WIN32
+			system("cls");
+#else
+			system("clear");
+#endif
+		}
 	};
 
 }
