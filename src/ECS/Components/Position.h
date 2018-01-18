bd312863e233b28bd4e914533758618c8c43bf14
KonH <konh@yandex.ru>
2018-01-18 22:23:02 +0700

    Position extensions;

--- src/ECS/Components/Position.h
+++ src/ECS/Components/Position.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <iostream>
 #include "../Component.h"
 
 namespace ECS {
@@ -6,10 +7,44 @@ namespace ECS {
 		int X;
 		int Y;
 
-		Position(int x, int y):Component() {
+		Position(int x, int y) :
+				Component() {
 			X = x;
 			Y = y;
 		}
 	};
+
+	ostream& operator <<(ostream& s, const Position& p) {
+		if ( s ) {
+			s << "{" << p.X << ", " << p.Y << "}";
+		}
+		return s;
+	}
+
+	bool operator ==(const Position& lhr, const Position& rhr) {
+		return (lhr.X == rhr.X) && (lhr.Y == rhr.Y);
+	}
+
+	bool operator !=(const Position& lhr, const Position& rhr) {
+		return !(lhr == rhr);
+	}
+
+	bool operator >(const Position& a, const Position& b) {
+		if (a.Y > b.Y) {
+			return true;
+		} else if (a.Y == b.Y) {
+			return a.X > b.X;
+		}
+		return false;
+	}
+
+	bool operator <(const Position& a, const Position& b) {
+		if (a.Y < b.Y) {
+			return true;
+		} else if (a.Y == b.Y) {
+			return a.X < b.X;
+		}
+		return false;
+	}
 }
 
