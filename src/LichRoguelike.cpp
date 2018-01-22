1e916cdedd4799a9a64d4344819d452942ae4054
KonH <konh@yandex.ru>
2018-01-22 21:50:08 +0700

    Simple example;

--- src/LichRoguelike.cpp
+++ src/LichRoguelike.cpp
@@ -2,6 +2,8 @@
 
 #include "ECS/Engine.h"
 #include "ECS/Tests.h"
+#include "ECS/Systems/PlayerMoveSystem.h"
+#include "ECS/Systems/MoveSystem.h"
 #include "ECS/Systems/RenderSystem.h"
 
 using namespace ECS;
@@ -9,6 +11,23 @@ using namespace std;
 
 int main() {
 	TestAll();
+
+	auto entities = make_shared<EntityManager>();
+
+	auto player = entities->CreateEntity();
+	player->AddComponent(Position(5, 5));
+	player->AddComponent(Movable());
+	player->AddComponent(Player());
+	player->AddComponent(View('@'));
+
+	Engine engine(entities);
+	engine.AddSystem(make_shared<PlayerMoveSystem>());
+	engine.AddSystem(make_shared<MoveSystem>());
+	engine.AddSystem(make_shared<RenderSystem>(cout));
+
+	while(true){
+		engine.Update();
+	}
 
 	return 0;
 }
